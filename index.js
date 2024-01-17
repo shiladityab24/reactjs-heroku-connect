0c6fe0a2601186bc37b4f7c1be428ffd06bb70e7
Shiladitya Bose <shiladitya_bose@persistent.com>
2024-01-17 18:14:45 +0530

    reactjs-heroku-connect

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyParser = require("body-parser")
+const path = require('path');
+const cors = require('cors')
+
+const app = express()
+var corsOptions = {
+    origin: "*",
+  };
+app.use(cors(corsOptions))
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Listening to port 3000")
+})
